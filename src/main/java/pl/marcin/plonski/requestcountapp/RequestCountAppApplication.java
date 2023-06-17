c8fa603bffaa6fac5c45357f8e8591dc6238eef2
mmplonski <mplonski20@wp.pl>
2023-06-17 09:51:06 +0200

    Init project

---
+++ src/main/java/pl/marcin/plonski/requestcountapp/RequestCountAppApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.plonski.requestcountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCountAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestCountAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/marcin/plonski/requestcountapp/RequestCountAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.plonski.requestcountapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestCountAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
